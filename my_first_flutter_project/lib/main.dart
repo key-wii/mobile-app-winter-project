52842ef8a8e0bd9acd4810140ed000b0bff1c3a5
Christopher <programmerchristopher@gmail.com>
2022-01-12 12:36:08 -0800

    image picker, static gallery

--- my_first_flutter_project/lib/main.dart
+++ my_first_flutter_project/lib/main.dart
@@ -1,11 +1,16 @@
-import 'package:flutter/material.dart';
+//import 'dart:ffi';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+import 'mynextscreen.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -13,33 +18,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -48,68 +35,138 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+  final urlImages = [
+    'https://images.unsplash.com/photo-1456926631375-92c8ce872def?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGVvcGFyZHN8ZW58MHx8MHx8&w=1000&q=80',
+    'https://images.unsplash.com/photo-1561731216-c3a4d99437d5?ixlib=rb-1.2.1'
+  ];
+
+  bool empty = true;
+  late XFile? _Ximage;
+  late File _image;
+
+  Future getImageFromCamera() async {
+    var image = await ImagePicker().pickImage(source: ImageSource.camera);
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      empty = false;
+      _Ximage = image;
+      _image = File(_Ximage!.path);
+    });
+  }
+
+  Future getImageFromGallery() async {
+    var image = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      empty = false;
+      _Ximage = image;
+      _image = File(_Ximage!.path);
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+  Widget build(BuildContext context) => Scaffold(
+    body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Center(
+          child: Text(
+            "Image Picker",
+            style: TextStyle(fontSize: 30),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 200.0,
+            child: Center(
+              child: empty == true ? Text('No image') : Image.file(_image),
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            FloatingActionButton(
+              onPressed: getImageFromCamera,
+              tooltip: "Pick Image form gallery",
+              child: Icon(Icons.add_a_photo),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            FloatingActionButton(
+              onPressed: getImageFromGallery,
+              tooltip: "Pick Image from camera",
+              child: Icon(Icons.camera_alt),
+            )
           ],
         ),
+        Center(
+          child: InkWell(
+            child: Ink.image(
+              image: NetworkImage(urlImages[0]),
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            onTap: openGallery,
+          ),
+        ),
+      ],
+    ),
+  );
+
+  void openGallery() => Navigator.of(context).push(MaterialPageRoute(
+      builder: (_) => GalleryWidget(
+        urlImages: urlImages,
+        index: 0,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  ));
+}
+
+class GalleryWidget extends StatefulWidget {
+  final PageController pageController;
+  final List<String> urlImages;
+  final int index;
+
+  GalleryWidget({
+    required this.urlImages,
+    this.index = 0,
+  }) : pageController = PageController(initialPage: index);
+
+  @override
+  State<StatefulWidget> createState() => _GalleryWidgetState();
+}
+
+class _GalleryWidgetState extends State<GalleryWidget> {
+  late int index = widget.index;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: Stack(
+      alignment: Alignment.bottomLeft,
+      children: [
+        PhotoViewGallery.builder(
+          pageController: widget.pageController,
+          itemCount: widget.urlImages.length,
+          builder: (context, index) {
+            final urlImage = widget.urlImages[index];
+
+            return PhotoViewGalleryPageOptions(
+              imageProvider: NetworkImage(urlImage),
+              minScale: PhotoViewComputedScale.contained,
+              maxScale: PhotoViewComputedScale.contained * 4,
+            );
+          },
+          onPageChanged: (index) => setState(() => this.index = index),
+        ),
+        Container(
+          padding: EdgeInsets.all(16),
+          child: Text(
+            'Image ${index + 1}/${widget.urlImages.length}',
+            style: TextStyle(color: Colors.white, fontSize: 1)
+          ),
+        ),
+      ],
+    ),
+  );
 }
